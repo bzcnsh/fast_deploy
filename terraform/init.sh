48111086af2dc2b2a5a16f42e4c7fae1be3cc193
Yimin Zheng <yiminzheng@Yimins-MacBook-Pro-2.local>
2018-10-10 11:51:31 -0400

    terraform bugfix, persist env var

--- terraform/init.sh
+++ terraform/init.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 export ROLE=##ROLE##
 export DB_IP=##DB_IP##
+env | grep "ROLE\|DB_IP" > /tmp/init_envvars
 sudo apt-get update
 sudo apt-get install -y git
 git clone https://github.com/bzcnsh/fast_deploy.git
